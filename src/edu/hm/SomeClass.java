f96813c16b62a046be459b329a6a88bc0f3f2ddc
Ivana Colic <ivi.colic@hotmail.com>
2017-04-08 10:23:06 +0200

    Add example 'SomeClass'

---
+++ src/edu/hm/SomeClass.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+/**
+ * Created by ivi on 4/8/2017.
+ */
+public class SomeClass {
+    @RenderMe private int foo;
+    @RenderMe(with="edu.hm.renderer.ArrayRenderer") int[] array = {1, 2, 3, };
+    @RenderMe private Date date = new Date(123456789);
+
+    public SomeClass(int foo) {
+        this.foo = foo;
+    }
+}
